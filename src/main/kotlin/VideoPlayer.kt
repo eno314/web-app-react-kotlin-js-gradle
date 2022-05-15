df9dcd3d4aa4e1a1f54caa4a2bc30bee720bb9ae
Hiroto Kitamura <kirikonknow@gmail.com>
2022-05-15 14:57:26 +0900

    ch7

--- src/main/kotlin/VideoPlayer.kt
+++ src/main/kotlin/VideoPlayer.kt
@@ -8,7 +8,6 @@ import react.css.css
 import react.dom.html.ReactHTML.button
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.h3
-import react.dom.html.ReactHTML.img
 
 external interface VideoPlayerProps : Props {
     var video: Video
@@ -44,8 +43,28 @@ val VideoPlayer = FC<VideoPlayerProps> { props ->
                 +"Mark as unwatched"
             }
         }
-        img {
-            src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+        div {
+            css {
+                display = Display.flex
+                marginBottom = 10.px
+            }
+            EmailShareButton {
+                url = props.video.videoUrl
+                EmailIcon {
+                    size = 32
+                    round = true
+                }
+            }
+            TelegramShareButton {
+                url = props.video.videoUrl
+                TelegramIcon {
+                    size = 32
+                    round = true
+                }
+            }
+        }
+        ReactPlayer {
+            url = props.video.videoUrl
         }
     }
 }
